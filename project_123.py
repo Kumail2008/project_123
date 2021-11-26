eb3602166c5847a6ab30251b2a25cc70febfd384
Kumail2008 <69812462+Kumail2008@users.noreply.github.com>
2021-11-26 09:28:12 -0500

    Add files via upload

--- project_123.py
+++ project_123.py
@@ -18,7 +18,7 @@ import os,ssl,time
 X,y = fetch_openml('mnist_784',version = 1, return_X_y = True)
 print(pd.Series(y).value_counts()) 
 
-classes = ['0','1','2','3','4','5','6','7','8','9']
+classes = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','Y','X','Z']
 nclasses = len(classes)
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,random_state = 9,train_size = 7500, test_size = 2500)
